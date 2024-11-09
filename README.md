af7d31d63549d390675cae044133508c3d3979a3
Lncvrt <lncvrtreal@gmail.com>
2024-11-09 11:16:43 -0700

    x86 support

--- README.md
+++ README.md
@@ -1,26 +1,54 @@
 # Password Generator
 
-A cross-platform password generator written in C, designed for both Windows and Linux, with support for x64 architecture only.
+A cross-platform password generator written in C, designed for both Windows and Linux, with support for x64 and x86.
 
 ## Building
 
 ### On Linux:
+
 To build the application, run the `build.sh` script. It will generate executables for both Linux and Windows in the `build/` directory.
 
 ```bash
 ./build.sh
 ```
 
+If it fails, install the required dependencies.
+
+```
+sudo apt update
+sudo apt install -y build-essential gcc mingw-w64 zip
+```
+
 ### Manually (if not using `build.sh`):
+
 You can manually compile the application on Linux with the following steps:
 
+#### x64 version
+
 Use the following command to compile for Linux:
-   ```bash
-   gcc -m64 -O0 -g3 -fno-inline -funroll-loops -flto -ftree-vectorize src/main.c -o "Password Generator"
-   ```
+
+```bash
+gcc -m64 -O0 -g3 -fno-inline -funroll-loops -flto -ftree-vectorize src/main.c -o "Password Generator"
+```
+
 To compile for Windows, you can cross-compile with tools like MinGW. The command may look like this:
-   ```bash
-   x86_64-w64-mingw32-gcc -m64 -O0 -g3 -fno-inline -funroll-loops -flto -ftree-vectorize src/main.c -o "Password Generator.exe"
-   ```
+
+```bash
+x86_64-w64-mingw32-gcc -m64 -O0 -g3 -fno-inline -funroll-loops -flto -ftree-vectorize src/main.c -o "Password Generator.exe"
+```
+
+#### x86 version
+
+Use the following command to compile for Linux:
+
+```bash
+gcc -m32 -O0 -g3 -fno-inline -funroll-loops -flto -ftree-vectorize src/main.c -o "Password Generator"
+```
+
+To compile for Windows, you can cross-compile with tools like MinGW. The command may look like this:
+
+```bash
+i686-w64-mingw32-gcc -O0 -g3 -fno-inline -funroll-loops -flto -ftree-vectorize src/main.c -o "build/windows/x86/Password Generator.exe"
+```
 
 This should generate executables for both Linux and Windows.
--- build.sh
+++ build.sh
@@ -5,23 +5,46 @@ if [ "$can_windows" == "false" ]; then
     echo "Can't compile windows version due to no MinGW detected"
 fi
 
+rm -rf build
 mkdir -p build
 mkdir -p build/linux
+mkdir -p build/linux/x64
+mkdir -p build/linux/x86
 mkdir -p build/windows
-rm -f build/linux/*
-rm -f build/windows/*
+mkdir -p build/windows/x64
+mkdir -p build/windows/x86
 
-gcc -m64 -O0 -g3 -fno-inline -funroll-loops -flto -ftree-vectorize src/main.c -o "build/linux/Password Generator"
+gcc -m64 -O0 -g3 -fno-inline -funroll-loops -flto -ftree-vectorize src/main.c -o "build/linux/x64/Password Generator"
 if [ $? -ne 0 ]; then
-    echo "Failed to link the Linux executable"
+    echo "Failed to link the Linux x64 executable"
     exit 1
+else
+    zip -r -9 build/linux-x64.zip build/linux/x64/Password\ Generator
+fi
+
+gcc -m32 -O0 -g3 -fno-inline -funroll-loops -flto -ftree-vectorize src/main.c -o "build/linux/x86/Password Generator"
+if [ $? -ne 0 ]; then
+    echo "Failed to link the Linux x86 executable"
+    exit 1
+else
+    zip -r -9 build/linux-x86.zip build/linux/x86/Password\ Generator
 fi
 
 if [ "$can_windows" == "true" ]; then
-    x86_64-w64-mingw32-gcc -m64 -O0 -g3 -fno-inline -funroll-loops -flto -ftree-vectorize src/main.c -o "build/windows/Password Generator.exe"
+    x86_64-w64-mingw32-gcc -m64 -O0 -g3 -fno-inline -funroll-loops -flto -ftree-vectorize src/main.c -o "build/windows/x64/Password Generator.exe"
     if [ $? -ne 0 ]; then
-        echo "Failed to compile Windows executable"
+        echo "Failed to compile Windows x64 executable"
         exit 1
+    else
+        zip -r -9 build/windows-x64.zip build/linux/x64/Password\ Generator
+    fi
+
+    i686-w64-mingw32-gcc -O0 -g3 -fno-inline -funroll-loops -flto -ftree-vectorize src/main.c -o "build/windows/x86/Password Generator.exe"
+    if [ $? -ne 0 ]; then
+        echo "Failed to compile Windows x32 executable"
+        exit 1
+    else
+        zip -r -9 build/windows-x86.zip build/linux/x86/Password\ Generator
     fi
 fi
 
